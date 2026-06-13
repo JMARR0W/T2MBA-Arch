900632692ed88db19ffabff3fb11834bf24678ac
JMARR0W <83468490+JMARR0W@users.noreply.github.com>
2026-06-13 12:13:05 -0700

    adding config files

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,15 @@
+-- treesitter.lua
+require("lazy").setup({{
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "hyprlang" },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+    end
+ }})
